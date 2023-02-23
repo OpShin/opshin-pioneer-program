3617a77bf4eb47083e192981ef05f5bbda619486
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-02-22 23:45:22 -0500

    implement week02 homework plutus scripts

---
+++ code/week02/homework/homework1.py
@@ -0,0 +1,11 @@
+from eopsin.prelude import *
+
+# `Tuple` is not supported yet
+# This should validate if and only if the two Booleans in the redeemer are True!
+# def validator(v: None, r: Tuple[bool, bool], c: ScriptContext) -> bool:
+#     return False
+
+
+# This should validate if and only if the Booleans in the redeemer list are True!
+def validator(v: None, r: List[bool], c: ScriptContext) -> bool:
+    return False
---
+++ code/week02/homework/homework2.py
@@ -0,0 +1,12 @@
+from eopsin.prelude import *
+
+
+@dataclass()
+class MyRedeemer(PlutusData):
+    flag1: bool
+    flag2: bool
+
+
+# Create a validator that unlocks the funds if MyRedemeer's flags are different
+def validator(v: None, r: MyRedeemer, c: ScriptContext) -> bool:
+    return False
