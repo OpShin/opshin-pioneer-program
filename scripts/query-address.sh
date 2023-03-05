44778627ed42106d93480bdbabeb60a27766b3b1
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-03-05 18:20:56 -0500

    use docker command

--- scripts/query-address.sh
+++ scripts/query-address.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cardano-cli query utxo --address "$1" --testnet-magic 2
+cardano_cli="docker run --rm -it \
+  --entrypoint cardano-cli \
+  -e CARDANO_NODE_SOCKET_PATH=/ipc/node.socket \
+  -v eopsin-pioneer-program_node-db:/data \
+  -v eopsin-pioneer-program_node-ipc:/ipc \
+  inputoutput/cardano-node"
+$cardano_cli query utxo --address "$1" --testnet-magic 2
--- scripts/query-tip.sh
+++ scripts/query-tip.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cardano-cli query tip --testnet-magic 2
+cardano_cli="docker run --rm -it \
+  --entrypoint cardano-cli \
+  -e CARDANO_NODE_SOCKET_PATH=/ipc/node.socket \
+  -v eopsin-pioneer-program_node-db:/data \
+  -v eopsin-pioneer-program_node-ipc:/ipc \
+  inputoutput/cardano-node"
+$cardano_cli query tip --testnet-magic 2
