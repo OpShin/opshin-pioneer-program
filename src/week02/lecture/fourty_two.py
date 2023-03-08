415d0531b7c933ccd5edc9fa9a70e561afa65b04
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-03-08 02:16:12 -0500

    make more like plutus lecture script

--- src/week02/lecture/fourty_two.py
+++ src/week02/lecture/fourty_two.py
@@ -1,9 +1,6 @@
 from eopsin.prelude import *
 
-# `isinstance` is not available yet: https://github.com/ImperatorLang/eopsin/issues/44
-# def validator(datum: BuiltinData, redeemer: BuiltinData, context: BuiltinData) -> None:
-#     assert isinstance(redeemer, int) and redeemer == 42
 
-
-def validator(datum: BuiltinData, redeemer: int, context: BuiltinData) -> None:
-    assert redeemer == 42
+def validator(datum: BuiltinData, redeemer: BuiltinData, context: BuiltinData) -> None:
+    r: int = redeemer
+    assert r == 42
