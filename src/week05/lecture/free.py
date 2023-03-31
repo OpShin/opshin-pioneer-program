9c3434d177c2cb1a18dcca5ec6e01b40cde07998
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-03-30 23:54:34 -0400

    create lecture scripts

---
+++ src/week05/lecture/free.py
@@ -0,0 +1,14 @@
+from opshin.prelude import *
+
+
+def assert_minting_purpose(context: ScriptContext) -> None:
+    purpose = context.purpose
+    if isinstance(purpose, Minting):
+        is_minting = True
+    else:
+        is_minting = False
+    assert is_minting, "not minting purpose"
+
+
+def validator(datum: None, redeemer: None, context: ScriptContext) -> None:
+    assert_minting_purpose(context)
