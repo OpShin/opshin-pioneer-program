72bd0549f1ac628352ad0143aedade5ecec6b32d
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-03-31 23:17:01 -0400

    fix validation errors when zero amount token is included

--- src/week05/lecture/nft.py
+++ src/week05/lecture/nft.py
@@ -17,15 +17,16 @@ def has_utxo(context: ScriptContext, oref: TxOutRef) -> bool:
 def check_minted_amount(tn: TokenName, context: ScriptContext) -> bool:
     mint_value = context.tx_info.mint
     valid = False
-    if len(mint_value.keys()) == 1:
-        for policy_id in mint_value.keys():
-            v = mint_value.get(policy_id, {b"": 0})
-            if len(v.keys()) == 1:
-                for token_name in v.keys():
-                    amount = v.get(token_name, 0)
-                    valid = token_name == tn and amount == 1
-                    print(str(amount))
-    return valid
+    count = 0
+    for policy_id in mint_value.keys():
+        v = mint_value.get(policy_id, {b"": 0})
+        if len(v.keys()) == 1:
+            for token_name in v.keys():
+                amount = v.get(token_name, 0)
+                valid = token_name == tn and amount == 1
+                if amount != 0:
+                    count += 1
+    return valid and count == 1
 
 
 def validator(
