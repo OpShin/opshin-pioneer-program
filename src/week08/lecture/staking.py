a588058062f5640a84498bf9dae5016d8f5fd7c8
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-05-03 21:14:58 -0400

    use all_tokens_locked_at_address

--- src/week08/lecture/staking.py
+++ src/week08/lecture/staking.py
@@ -12,21 +12,15 @@ def get_amount(context: ScriptContext, purpose: Rewarding) -> int:
     return amount
 
 
-def paid_to_address(addr: Address, context: ScriptContext) -> int:
-    paid_amount = 0
-    for output in context.tx_info.outputs:
-        if output.address == addr:
-            paid_amount += output.value.get(b"", {b"": 0}).get(b"", 0)
-    return paid_amount
-
-
 def validator(addr: Address, _: None, context: ScriptContext):
     purpose = context.purpose
     if isinstance(purpose, Certifying):
         print("certifying")
     elif isinstance(purpose, Rewarding):
         amount = get_amount(context, purpose)
-        paid_amount = paid_to_address(addr, context)
+        paid_amount = all_tokens_locked_at_address(
+            context.tx_info.outputs, addr, Token(b"", b"")
+        )
         assert 2 * paid_amount >= amount, "insufficient reward sharing"
     else:
         assert False, "script purpose must be certifying or rewarding"
