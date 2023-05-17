f7a33d507643b7c329139c220793d2c354d598e4
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-05-17 19:42:44 -0400

    collateral script

--- src/week09/on_chain/collateral.py
+++ src/week09/on_chain/collateral.py
@@ -1,6 +1,5 @@
 from opshin.prelude import *
 
-
 CurrencySymbol = bytes
 
 
@@ -23,6 +22,31 @@ class Liquidate(PlutusData):
 
 CollateralRedeemer = Union[Redeem, Liquidate]
 
+STABLECOIN_TOKEN_NAME = b"USDP"
+
+
+def check_signed_by_owner(datum: CollateralDatum, context: ScriptContext) -> bool:
+    return datum.owner in context.tx_info.signatories
+
+
+def minted_amount(datum: CollateralDatum, context: ScriptContext) -> int:
+    minted = context.tx_info.mint
+    return minted.get(datum.minting_policy_id, {b"": 0}).get(STABLECOIN_TOKEN_NAME, 0)
+
+
+def check_stablecoin_amount(datum: CollateralDatum, context: ScriptContext) -> bool:
+    return -datum.stable_coin_amount == minted_amount(datum, context)
+
 
 def validator(datum: CollateralDatum, r: CollateralRedeemer, context: ScriptContext):
-    pass
+    if isinstance(r, Redeem):
+        assert check_signed_by_owner(
+            datum, context
+        ), "collateral owner's signature missing"
+        assert check_stablecoin_amount(
+            datum, context
+        ), "burned stablecoin amount mismatch"
+    elif isinstance(r, Liquidate):
+        assert check_stablecoin_amount(
+            datum, context
+        ), "burned stablecoin amount mismatch"
