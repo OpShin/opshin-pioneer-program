7c981d6f8df66a45fd5d0aca081e31be6316fd73
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-05-17 01:12:16 -0400

    nft script

--- src/week09/on_chain/nft.py
+++ src/week09/on_chain/nft.py
@@ -1,5 +1,33 @@
 from opshin.prelude import *
 
 
+def assert_minting_purpose(context: ScriptContext) -> None:
+    purpose = context.purpose
+    if isinstance(purpose, Minting):
+        is_minting = True
+    else:
+        is_minting = False
+    assert is_minting, "not minting purpose"
+
+
+def has_utxo(oref: TxOutRef, context: ScriptContext) -> bool:
+    return any([oref == i.out_ref for i in context.tx_info.inputs])
+
+
+def check_minted_amount(tn: TokenName, context: ScriptContext) -> bool:
+    mint_value = context.tx_info.mint
+    valid = False
+    count = 0
+    for policy_id, v in mint_value.items():
+        if len(v.keys()) == 1:
+            for token_name, amount in v.items():
+                valid = token_name == tn and amount == 1
+                if amount != 0:
+                    count += 1
+    return valid and count == 1
+
+
 def validator(oref: TxOutRef, tn: TokenName, _: None, context: ScriptContext):
-    pass
+    assert_minting_purpose(context)
+    assert has_utxo(oref, context), "UTxO not consumed"
+    assert check_minted_amount(tn, context), "wrong amount minted"
