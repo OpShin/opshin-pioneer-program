c1d80353aa279f090f0ce4e64e16c08124e3d468
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-05-17 20:03:55 -0400

    collateral script comments

--- src/week09/on_chain/collateral.py
+++ src/week09/on_chain/collateral.py
@@ -5,6 +5,8 @@ CurrencySymbol = bytes
 
 @dataclass()
 class CollateralDatum(PlutusData):
+    """Datum containing all the relevant information"""
+
     minting_policy_id: CurrencySymbol
     owner: PubKeyHash
     stable_coin_amount: int
@@ -20,21 +22,25 @@ class Liquidate(PlutusData):
     CONSTR_ID = 1049
 
 
+# We can lock or redeem our own collateral or liquidate someone else's
 CollateralRedeemer = Union[Redeem, Liquidate]
 
 STABLECOIN_TOKEN_NAME = b"USDP"
 
 
 def check_signed_by_owner(datum: CollateralDatum, context: ScriptContext) -> bool:
+    """Check if the transaction is signed by the collateral owner"""
     return datum.owner in context.tx_info.signatories
 
 
 def minted_amount(datum: CollateralDatum, context: ScriptContext) -> int:
+    """Amount of stablecoins minted in this transaction"""
     minted = context.tx_info.mint
     return minted.get(datum.minting_policy_id, {b"": 0}).get(STABLECOIN_TOKEN_NAME, 0)
 
 
 def check_stablecoin_amount(datum: CollateralDatum, context: ScriptContext) -> bool:
+    """Check that the amount of stablecoins burned matches the amont at the collateral's datum"""
     return -datum.stable_coin_amount == minted_amount(datum, context)
 
 
