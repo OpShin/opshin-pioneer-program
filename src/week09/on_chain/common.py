050d65c2b7e22bf44ba13b85003908408e5ebfa0
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2023-05-18 21:29:54 -0400

    implement oracle script

--- src/week09/on_chain/common.py
+++ src/week09/on_chain/common.py
@@ -1,36 +1,22 @@
 from opshin.prelude import *
 
 
+STABLECOIN_TOKEN_NAME = b"USDP"
+
+
 CurrencySymbol = bytes
 
 
 @dataclass()
 class CollateralDatum(PlutusData):
-    """Datum containing all the relevant information"""
-
     minting_policy_id: CurrencySymbol
     owner: PubKeyHash
     stable_coin_amount: int
 
 
-STABLECOIN_TOKEN_NAME = b"USDP"
-
-
 def parse_collateral_datum_unsafe(o: TxOut, info: TxInfo) -> CollateralDatum:
     datum: CollateralDatum = resolve_datum_unsafe(o, info)
     return datum
-
-
-@dataclass()
-class AssetClass(PlutusData):
-    currency_symbol: bytes
-    token_name: bytes
-
-
-@dataclass()
-class OracleParams(PlutusData):
-    oracle_nft: AssetClass
-    oracle_operator: PubKeyHash
 
 
 # Oracle Datum
