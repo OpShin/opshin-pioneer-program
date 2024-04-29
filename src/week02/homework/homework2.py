36fceaac9ba22d8367e8006c94efef4057b75741
Niels Mündler <n.muendler@posteo.de>
2024-04-29 11:44:14 +0200

    Fix

--- src/week02/homework/homework2.py
+++ src/week02/homework/homework2.py
@@ -3,10 +3,11 @@ from opshin.prelude import *
 
 @dataclass()
 class MyRedeemer(PlutusData):
+    CONSTR_ID = 0
     flag1: bool
     flag2: bool
 
 
 # Create a validator that unlocks the funds if MyRedemeer's flags are different
 def validator(v: None, r: MyRedeemer, c: ScriptContext) -> None:
-    assert False
+    assert False, "Not implemented yet!"
